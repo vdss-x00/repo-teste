63367d940cbac494e90a17ddc021de5c3206d551
Vitor <195686982+vdss-x00@users.noreply.github.com>
2026-08-18 20:35:28 -0300

    Quarto Commit + Mudanças Adicionais

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,52 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.status(200).json({
         message: "Sistema Plataforma Philia",
     })
 })
 
+const campanhas = [
+    {
+        id: 1,
+        nomeFundador: "Nome1",
+        categoria: "Tragédia Humanitária",
+        descricao: "Doe para ajudar as pessoas afetadas!",
+        aceitaDoacao: true
+    },
+    {
+        id: 2,
+        nomeFundador: "Nome2",
+        categoria: "Doença",
+        descricao: "",
+        aceitaDoacao: true
+    }
+];
+
+const blog = [
+    {
+        titulo1: "Quem Somos",
+        paragrafo: "Nós somos uma plataforma de angariação de fundos para as mais variadas causas. Seja para ajudar vítimas de desastres naturais, pacientes de doenças, ou outros, estamos sempre aptos para ajudar!"
+    }
+]
+
 app.get("/sobre-nos", (req, res) => {
-    res.status(200).json({
-        message: "Nosso Blog",
-    })
+    res.status(200).json(blog)
 })
 
 app.get("/campanhas", (req, res) => {
-    res.status(200).json({
-        message: "Veja Nossas Campanhas",
-    })
+    res.status(200).json(campanhas)
+})
+
+app.post("/campanhas", (req, res) => {
+    const campaign = req.body;
+
+    campanhas.push(campaign);
+
+    res.status(201).json(campaign)
 })
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,7 @@
-import http from "node:http";
+import app from "./app.js"
 
 const port = 3000;
 
-const routes = {
-    "/": {
-        message: "Sistema Plataforma Philia",
-    },
-    "/sobre-nos": {
-        message: "Nosso Blog",
-    },
-    "/campanhas": {
-        message: "Veja Nossas Campanhas"
-    }
-};
-
-const server = http.createServer((req, res) => {
-    const response = routes[req.url as keyof typeof routes];
-
-    if (!response){
-        res.writeHead(404, {
-            "content-type": "application/json",
-        })
-
-        return res.end(
-            JSON.stringify({
-                message: "Rota não encontrada.",
-            }),
-        );
-    };
-
-    res.writeHead(200, {
-        "content-type": "application/json",
-    });
-
-    res.end(JSON.stringify(response));
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor executado em http://localhost:${port}`);
 })
